4cb86e33d1c33904bc44fc37cc4cf559ce1ca5f3
mrbullet <coolph0ne>
2024-02-18 13:04:24 +0100

    wsgi

--- sobota_2_17/wsgi.py
+++ sobota_2_17/wsgi.py
@@ -6,35 +6,11 @@
 # For more information on this file, see
 # https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 # """
-#
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sobota_2_17.settings")
-#
-# application = get_wsgi_application()
 
-
-# +++++++++++ DJANGO +++++++++++
-# To use your own django app use code like this:
 import os
-import sys
 
-#
-## assuming your django settings file is at '/home/nazwauzytkownika/mysite/mysite/settings.py'
-## and your manage.py is is at '/home/nazwauzytkownika/mysite/manage.py'
-path = '/home/coolph0ne/pythonStudy/manage.py'
-if path not in sys.path:
-    sys.path.append(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'sobota_2_17.settings'
-#
-## then:
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sobota_2_17.settings")
+
 application = get_wsgi_application()
-##
-#import django.core.handlers.wsgi
-#
-#application = django.core.handlers.wsgi.WSGIHandler()
