2767cd458b72bbfaa2b59e6e12457db4d245dfcc
mrbullet <coolph0ne>
2024-02-18 13:24:35 +0100

    wsgi

--- sobota_2_17/wsgi.py
+++ sobota_2_17/wsgi.py
@@ -7,10 +7,22 @@
 # https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 # """
 
+#import os
+#
+#from django.core.wsgi import get_wsgi_application
+#
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sobota_2_17.settings")
+#
+#application = get_wsgi_application()
+
 import os
+import sys
+
+path = '/home/coolph0ne/pythonStudy'
+if path not in sys.path:
+    sys.path.insert(0,path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] == 'sobota_2_17.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sobota_2_17.settings")
-
 application = get_wsgi_application()
